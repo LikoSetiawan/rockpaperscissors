86964fe9d8a77244423122936ab96ed90cc7df09
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-02-06 15:56:53 +0800

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Liko Setiawan on 06/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
